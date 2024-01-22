5f1927f6b9c01ae7c92de03b6bcae3322b8f57ee
eunhye11 <eunhye11@chunjae.co.kr>
2024-01-23 08:16:23 +0900

    인코딩, 해싱, 암호화

--- learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -32,4 +36,49 @@ public class BasicAuthSecurityConfiguration {
 		
 		return http.build();
 	}
+	
+	//여러 사용자를 생성 - 메모리에 모든 정보를 생성하다.
+	@Bean
+	public UserDetailsService userDetailService() {
+		
+		var user = User.withUsername("in28minute")
+			//.password("{noop}dummy")
+			.password("dummy")
+			.passwordEncoder(str -> passwordEncoder().encode(str))
+			.roles("USER")
+			.build();
+		var admin = User.withUsername("admin")
+//				.password("{noop}dummy")
+				.password("dummy")
+				.passwordEncoder(str -> passwordEncoder().encode(str))
+				.roles("ADMIN")
+				.build();
+	
+		
+		return new InMemoryUserDetailsManager(user, admin);
+	}
+	
+	//bcrypt 사용 BCryptPasswordEncoder: 강력한 해싱 함수 BCrypt를 사용하는 PasswordEncoder
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
